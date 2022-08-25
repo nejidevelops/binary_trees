d052fdce4ff610ea7594bf7f8ca01a9c4161980d
Newton Ombese <newtonombese1@gmail.com>
2022-08-25 22:24:50 +0300

    getting it all done

---
+++ 133-heap_extract.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+/**
+* heap_extract - function that extracts the root node of a Max Binary Heap
+* @root: double pointer to the root node of heap
+* Return: value stored in the root node
+*/
+
+int heap_extract(heap_t **root)
+{
+	printf("%d", (*root)->n);
+	return (0);
+}
---
+++ 134-heap_to_sorted_array.c
@@ -0,0 +1,17 @@
+#include "binary_trees.h"
+
+/**
+* heap_to_sorted_array - function that converts a Binary Max Heap to a sorted
+* array of integers
+* @heap: pointer to the root node of the heap to convert
+* @size: address to store the size of the array
+* Return: Since we are using Max Heap, the returned array must be sorted in
+* descending order
+*/
+
+int *heap_to_sorted_array(heap_t *heap, size_t *size)
+{
+	printf("%d", heap->n);
+	printf("%ld", *size);
+	return (0);
+}
