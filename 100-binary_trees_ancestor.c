0f483e748b11e62c364930646e27d0c731dca597
Newton Ombese <newtonombese1@gmail.com>
2022-08-25 22:15:54 +0300

    fixing the check fail 10

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -1,55 +1,26 @@
 #include "binary_trees.h"
-binary_tree_t *recurse_for_ancestor(binary_tree_t *root,
-	const binary_tree_t *first, const binary_tree_t *second);
+
 /**
- * binary_trees_ancestor - find lowest common ancestor
- * @first: first node to find ancestor for
- * @second: second node to find ancestor for
+ * binary_trees_ancestor - Finds the lowest common ancestor of two nodes.
+ * @first: Pointer to the first node.
+ * @second: Pointer to the second node.
  *
- * Return: pointer to lowest common ancestor; NULL on failure
+ * Return: If no common ancestors return NULL, else return common ancestor.
  */
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
-	const binary_tree_t *second)
+		const binary_tree_t *second)
 {
-	binary_tree_t *root = NULL;
+	binary_tree_t *mom, *pop;
 
 	if (!first || !second)
 		return (NULL);
+	if (first == second)
+		return ((binary_tree_t *)first);
 
-	/* find root node */
-	root = (binary_tree_t *)first;
-	while (root->parent != NULL)
-		root = root->parent;
-
-	/* perform lowest common anscestor detection */
-	return (recurse_for_ancestor(root, first, second));
-}
-/**
- * recurse_for_ancestor - recursive component to find LCA
- * @root: pointer to root of tree
- * @first: pointer to one child
- * @second: pointer to other child
- *
- * Return: pointer to LCA
- */
-binary_tree_t *recurse_for_ancestor(binary_tree_t *root,
-	const binary_tree_t *first, const binary_tree_t *second)
-{
-	binary_tree_t *leftLCA = NULL, *rightLCA = NULL;
-
-	if (root == NULL || first == NULL || second == NULL)
-		return (NULL);
-
-	if (root == first || root == second)
-		return (root);
-
-	leftLCA = recurse_for_ancestor(root->left, first, second);
-	rightLCA = recurse_for_ancestor(root->right, first, second);
-
-	if (leftLCA && rightLCA)
-		return (root);
-
-	if (leftLCA != NULL)
-		return (leftLCA);
-	return (rightLCA);
+	mom = first->parent, pop = second->parent;
+	if (first == pop || !mom || (!mom->parent && pop))
+		return (binary_trees_ancestor(first, pop));
+	else if (mom == second || !pop || (!pop->parent && mom))
+		return (binary_trees_ancestor(mom, second));
+	return (binary_trees_ancestor(mom, pop));
 }
